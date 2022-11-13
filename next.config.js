acf67cd3665069bb1e1be9c84c1c5da4224b5fe9
sidlingappa <sidlingappak@gmail.com>
2022-11-13 21:18:09 +0530

    Added cors header

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/.*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
