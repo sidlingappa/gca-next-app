389ff4f8871d3bd6b57d89df4286db45cf55be59
sidlingappa <sidlingappak@gmail.com>
2022-11-13 21:15:23 +0530

    Added cors header

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   swcMinify: true,
   env: {
     API_URL: process.env.GCA_AUTH_URL,
+  },
+  async headers() {
+    return [
+      {
+        source: "/.*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
